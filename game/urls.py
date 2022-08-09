919ebb1bc6d04eb18afb554cc2cbc7780740ae17
xfang04 <fangxin1996@gmail.com>
2022-08-10 05:46:14 +0800

    grammer fix

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from game.views import index, play
 
-urlpatterns = {
-    path("", index, name="index"),
-    path("play/",play,name="play")
-}
+urlpatterns = {path("", index, name="index"), path("play/", play, name="play")}
--- game/views.py
+++ game/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 
 
@@ -8,8 +8,11 @@ def index(request):
     line3 = '<a href="/play/">进入游戏界面</a>'
     return HttpResponse(line1 + line2 + line3)
 
+
 def play(request):
     line1 = '<h1 style="text-align:center">游戏界面</h1>'
     line2 = '<a href="/">返回主界面</a>'
     return HttpResponse(line1 + line2)
+
+
 # Create your views here.
