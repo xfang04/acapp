3b0ba9707e4433258be2d45d81d0dd10a7928ce2
xfang04 <fangxin1996@gmail.com>
2022-08-12 02:07:58 +0800

    fix css

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -1,5 +1,16 @@
+class AcGameMenu{
+    constructor(root) {
+        this.root = root;
+        this.$menu = $(`
+        <div class="ac-game-menus"</div>
+        `);
+        this.root.$ac_game.append(this.$menu);
+    }
+}
 class AcGame {
     constructor(id) {
-
+        this.id = id;
+        this.$ac_game = $('#' +id);
+        this.menu = new AcGameMenu(this);
     }
 }
