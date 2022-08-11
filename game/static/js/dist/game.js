6c251444e182adc17b82cd7fe34821d7b27912ee
xfang04 <fangxin1996@gmail.com>
2022-08-12 02:35:12 +0800

    打包css测试

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -1,10 +1,25 @@
-class AcGameMenu{
+class AcGameMenu {
     constructor(root) {
         this.root = root;
         this.$menu = $(`
-        <div class="ac-game-menu"</div>
-        `);
+<div class="ac-game-menu"
+    <div class="ac-game-menu-field">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+            单人模式
+        </div>
+        <div class="ac-game-menu-field-item ac-game-menu-field-multi">
+            多人模式
+        </div>
+        <div class="ac-game-menu-field-item ac-game-menu-field-settings">
+            设置
+        </div>
+    </div>
+</div>
+`);
         this.root.$ac_game.append(this.$menu);
+        this.$single = this.$menu.find('.ac-game-menu-field-item-single');
+        this.$multi = this.$menu.find('.ac-game-menu-field-item-multi');
+        this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
     }
 }
 class AcGame {
