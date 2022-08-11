e5c13191c626c26a5a9a8a972ee59c0823f3d104
xfang04 <xfang04@syr.edu>
2022-08-11 13:58:42 -0400

    update js and css

---
+++ game/static/js/src/menu/zbase.js
@@ -0,0 +1,9 @@
+class AcGameMenu{
+    constructor(root) {
+        this.root = root;
+        this.$menu = $(`
+        <div class="ac-game-menu"</div>
+        `);
+        this.root.$ac_game.append(this.$menu);
+    }
+}
