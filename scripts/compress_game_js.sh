c6301de553de5fbc0727788d3543e066a888b64d
xfang04 <fangxin1996@gmail.com>
2022-08-12 00:44:10 +0800

    更新js脚本

--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 JS_PATH=/home/xfang/acapp/game/static/js/
-JS_PATH_DIST=${JS_PATH}dist
-JS_PATH_SRC=${JS_PATH}src
+JS_PATH_DIST=${JS_PATH}dist/
+JS_PATH_SRC=${JS_PATH}src/
 
 find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
