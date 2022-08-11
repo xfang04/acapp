00b53fa39dc53fa0c63703a9b760b81568a88f2d
xfang04 <xfang04@syr.edu>
2022-08-11 15:14:36 -0400

    update js

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -4,11 +4,11 @@ class AcGameMenu {
         this.$menu = $(`
 <div class="ac-game-menu">
     <div class="ac-game-menu-field">
-        <div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
             单人模式
         </div>
         <br>
-        <div class="ac-game-menu-field-item ac-game-menu-field-multi">
+        <div class="ac-game-menu-field-item ac-game-menu-field-multi-mode">
             多人模式
         </div>
         <br>
@@ -19,8 +19,25 @@ class AcGameMenu {
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
-        this.$single = this.$menu.find('.ac-game-menu-field-item-single');
-        this.$multi = this.$menu.find('.ac-game-menu-field-item-multi');
+        this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
         this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
+
+        this.start();
+    }
+    start(){
+        this.add_listening_events();
+    }
+    add_listening_events(){
+        let outer = this;
+        this.$single_mode.click(function (){
+            console.log("click single mode");
+        })
+        this.$multi_mode.click(function (){
+            console.log("click multi mode");
+        })
+        this.$settings.click(function (){
+            console.log("click settings");
+        })
     }
 }
