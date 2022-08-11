8a5ea01b5eced53cadcfd0b18447ac8436cc40e3
xfang04 <xfang04@syr.edu>
2022-08-11 14:10:43 -0400

    fix

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -2,7 +2,7 @@ class AcGameMenu{
     constructor(root) {
         this.root = root;
         this.$menu = $(`
-        <div class="ac-game-menus"</div>
+        <div class="ac-game-menu"</div>
         `);
         this.root.$ac_game.append(this.$menu);
     }
