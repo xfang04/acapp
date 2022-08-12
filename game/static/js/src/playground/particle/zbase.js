0e0246e1bdff971cfeb631776a07252a6f1db14d
xfang04 <fangxin1996@gmail.com>
2022-08-13 02:25:29 +0800

    创建粒子特效

---
+++ game/static/js/src/playground/particle/zbase.js
@@ -0,0 +1,27 @@
+class Particle extends AcGameObject {
+    constructor(playground, x, y, radius, vx, vy, color, speed) {
+        super();
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.friction = 0.9;
+    }
+    start(){
+
+    }
+    update() {
+        this.render();
+    }
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+}
